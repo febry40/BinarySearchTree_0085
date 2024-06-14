39ebf839101f98638f8e2eb3fd98a2738b7254f5
febry40 <febryevayonisa@gmail.com>
2024-06-14 14:25:25 +0700

    fungsi ini mencari kode dari nodeyang ditentukan

--- BinarySearchTree_0085/BinarySearchTree_0085.cpp
+++ BinarySearchTree_0085/BinarySearchTree_0085.cpp
@@ -48,5 +48,14 @@ public:
 		{
 			parent->leftchild = newNode;
 		}
+		else if (element > parent->info)
+		{
+			parent->rightchild = newNode;
+		}
+	}
+
+	void search(string element, Node*& parent, Node*& currentNode)
+	{
+		currentNode = ROOT;
 	}
 };
